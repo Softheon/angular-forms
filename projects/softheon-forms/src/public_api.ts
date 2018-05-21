8f1f47876ab89de2f818f5aecb049aba10b0dc87
John Paul Pennisi <jpennisi@softheon.com>
2018-05-21 14:29:08 -0400

    Added provide parent form directive

--- projects/softheon-forms/src/public_api.ts
+++ projects/softheon-forms/src/public_api.ts
@@ -5,3 +5,5 @@
 export * from './lib/name/name.module';
 export * from './lib/social-security-number/social-security-number.module';
 export * from './lib/date-picker/date-picker.module';
+export * from './lib/directives/number-of-digits.directive';
+export * from './lib/directives/provide-parent-form.directive';
